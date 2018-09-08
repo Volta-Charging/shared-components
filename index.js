972c959548d3fa45162463c97ada58c7b0d126f5
Stefan Martin <ste00martin@gmail.com>
2018-09-07 17:02:17 -0700

    moving index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-import COLOR from './src/Color'
-
-export { default as SiteCluster } from './src/map/Cluster'
-export { default as SiteMarker } from './src/map/Marker'
-export default COLOR
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import COLOR from './Color'
+
+export { default as SiteCluster } from './map/Cluster'
+export { default as SiteMarker } from './map/Marker'
+export default COLOR
