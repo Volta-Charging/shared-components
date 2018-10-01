5fa02a9302b4033461ec01c0896df1866d108092
Judy Zaratan <judy.zaratan@gmail.com>
2018-10-01 11:30:14 -0700

    removes positionAbsolute prop in components

--- src/marker/Badge/styled.js
+++ src/marker/Badge/styled.js
@@ -3,11 +3,9 @@ import styled from 'styled-components'
 import COLOR from '../../Color'
 
 export const Notification = styled.div`
-  ${({ positionAbsolute }) => positionAbsolute && `
-    position: absolute;
-    top: 0px;
-    left: 0px;
-  `}
+  position: absolute;
+  top: 0px;
+  left: 0px;
   width: 20px;
   height: 20px;
   font-size: 12px;
