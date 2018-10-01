a14e7e83258350a95d9e54bc65ad051037b80aab
Judy Zaratan <judy.zaratan@gmail.com>
2018-10-01 12:48:05 -0700

    adds directories for linting checks

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,16 +1,14 @@
-const path = require("path");
-
 module.exports = {
   module: {
     rules: [
       {
-        test: /\.(eot|woff|woff2|svg|ttf|otf)([\?]?.*)$/,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]',
           outputPath: 'assets/fonts/',
         },
-      }
-    ]
-  }
-};
+        test: /\.(eot|woff|woff2|svg|ttf|otf)([\?]?.*)$/,
+      },
+    ],
+  },
+}
