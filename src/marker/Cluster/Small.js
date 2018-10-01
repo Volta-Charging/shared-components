c4fd8965a43746757236caf00eba31feb0f9a3fa
Judy Zaratan <judy.zaratan@gmail.com>
2018-09-30 22:44:03 -0700

    pie chart rendering

--- src/marker/Cluster/Small.js
+++ src/marker/Cluster/Small.js
@@ -1,18 +1,42 @@
 import React from 'react'
 
 import  NotificationBadge from '../Badge'
-
+import COLOR from '../../Color'
 import {
   Container,
   ClusterContainer,
   PieContainer,
   ClusterCount,
 } from './styled'
-
+import PieChart from '../PieChart'
 const SmallCluster = ({ text, size, onClick, issues }) => (
   <Container markerWidth={size}>
     <NotificationBadge issues={issues}/>
     <ClusterContainer onClick={onClick} markerWidth={size}>
+      <PieContainer>
+        <PieChart
+          fill={COLOR.GREY_DARK}
+          height={size - 10}
+          value={100}
+          width={size - 10}
+        />
+      </PieContainer>
+      <PieContainer>
+        <PieChart
+          fill={COLOR.BLUE_LIGHT}
+          height={size - 10}
+          value={80}
+          width={size - 10}
+        />
+      </PieContainer>
+      <PieContainer>
+        <PieChart
+          fill={COLOR.BLACK}
+          height={size - 21}
+          value={100}
+          width={size - 21}
+        />
+      </PieContainer>
       <PieContainer>
         <ClusterCount value={text}>{text}</ClusterCount>
       </PieContainer>
--- src/marker/Cluster/styled.js
+++ src/marker/Cluster/styled.js
@@ -2,6 +2,12 @@ import styled from 'styled-components'
 
 import COLOR from '../../Color'
 
+export const Container = styled.div`
+  width: ${({ markerWidth }) => markerWidth}px;
+  height: ${({ markerWidth }) => markerWidth}px;
+  padding: 8px;
+  position: relative;
+`
 export const ClusterContainer = styled.div`
   display: flex;
   align-items: center;
@@ -13,13 +19,20 @@ export const ClusterContainer = styled.div`
   border-width: 1px;
   border-radius: ${({ markerWidth }) => markerWidth / 2}px;
   background-color: ${COLOR.BLACK};
+  position: relative;
 `
 export const PieContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
 `
 export const ClusterCount = styled.span`
+  
   color: ${COLOR.WHITE};
   letter-spacing: -0.2px;
   font-size: ${({ value }) => value > 99 ? 14 : 17}px;
