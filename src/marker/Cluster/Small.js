9dfe6be15b3182b2b70c10d167bde5fb98669434
Judy Zaratan <judy.zaratan@gmail.com>
2018-10-01 12:05:44 -0700

    removes extraneous pie component rendering

--- src/marker/Cluster/Small.js
+++ src/marker/Cluster/Small.js
@@ -2,16 +2,17 @@ import React from 'react'
 
 import  NotificationBadge from '../Badge'
 import COLOR from '../../Color'
+import PieChart from '../PieChart'
+
 import {
   Container,
   ClusterContainer,
   PieContainer,
   ClusterCount,
 } from './styled'
-import PieChart from '../PieChart'
+
 const SmallCluster = ({ text, size, onClick, issues }) => (
   <Container markerWidth={size}>
-    <NotificationBadge issues={issues}/>
     <ClusterContainer onClick={onClick} markerWidth={size}>
       <PieContainer>
         <PieChart
@@ -41,6 +42,7 @@ const SmallCluster = ({ text, size, onClick, issues }) => (
         <ClusterCount value={text}>{text}</ClusterCount>
       </PieContainer>
     </ClusterContainer>
+    <NotificationBadge issues={issues}/>
   </Container>
 )
 
--- stories/index.js
+++ stories/index.js
@@ -4,8 +4,6 @@ import { storiesOf } from '@storybook/react'
 import Badge from '../src/marker/Badge'
 import Marker from '../src/marker/SiteMarker'
 import Cluster from '../src/marker/Cluster'
-import Pie from '../src/marker/PieChart'
-import COLOR from '../src/Color'
 
 storiesOf('Badge', module)
   .add('Issue', () => (
@@ -44,33 +42,17 @@ storiesOf('Cluster', module)
 
 storiesOf('Pie', module)
   .add('Pie', () => (
-    <div>
-
     <Cluster
-    text={1}
-    size={50}>
-      
+      text={1}
+      size={50}
+    >
     </Cluster>
-    <Pie
-    width={50}
-    height={50}
-    fill={COLOR.BLUE_LIGHT}
-    value={80}
+  ))
+  .add('With Notification Badge', ()=> (
+    <Cluster
+      issues={2}
+      text={1}
+      size={50}
     >
-  </Pie>
-  <Pie
-    width={30}
-    height={30}
-    fill={COLOR.GREY_DARK}
-    value={100}
-    >
-  </Pie>
-  <Pie
-    fill={COLOR.BLACK}
-    height={30 - 18}
-    marginBottom={12}
-    value={100}
-    width={30 - 18}
-    />
-    </div>
+    </Cluster>
   ))
