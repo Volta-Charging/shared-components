b40eefbf1299c1d186ae5e6fb12a754ae2c0eb9c
Judy Zaratan <judy.zaratan@gmail.com>
2018-10-03 12:18:32 -0700

    rendering charging pie on marker

--- src/marker/SiteMarker/styled.js
+++ src/marker/SiteMarker/styled.js
@@ -1,16 +1,36 @@
 import styled from 'styled-components'
 
+import COLOR from '../../Color'
+
+export const Container = styled.div`
+  width: ${({ markerWidth }) => markerWidth}px;
+  height: ${({ markerHeight }) => markerHeight}px;
+  position: relative;
+  padding: 9px;
+`
 export const MarkerContainer = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
   width: ${({ markerWidth }) => markerWidth}px;
   height: ${({ markerHeight }) => markerHeight}px;
   font-family: 'MalloryMP-Book';
-`
-export const Container = styled.div`
   position: relative;
-  width: ${({ markerWidth }) => markerWidth}px;
-  height: ${({ markerHeight }) => markerHeight}px;
-  padding: 8px;
 `
+
+export const PieContainer = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  justify-content: center;
+  top: 0px;
+  left: 0px;
+  width: ${({ width }) => width}px;
+  height:${({ height, marginBottom }) => height - marginBottom}px;
+`
+export const MarkerCount = styled.span`
+  display: inline-block;
+  color: ${COLOR.WHITE};
+  letter-spacing: -0.2px;
+  font-size: ${({ value }) => value > 99 ? 14 : 17}px;
+  font-family: 'MalloryMP-Book';
+`
+
